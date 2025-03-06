040f42754aa781e393c40ebe9dddf60c9b1a73d1
Quentin Loriaux <quentin.loriaux@gmail.com>
2025-03-06 02:48:48 +0100

    attempt to go jdbc

--- classement_par_reviews/filtre_note.py
+++ classement_par_reviews/filtre_note.py
@@ -7,18 +7,21 @@ spark = SparkSession.builder.appName("Steam_reviews").getOrCreate()
 df = spark.read.csv("../dataset/steam_reviews.csv", header=True, inferSchema=True)
 df.printSchema()
 
-
-reviews_score_good = df.filter(df.review_score == 1).select("review_text").limit(5)
+reviews_useful = df.filter(df.review_votes > 1).select("review_votes").orderBy("review_votes", ascending=False)
 print("review_score = 1:")
-reviews_score_good.show(truncate=False)
+reviews_useful.show(truncate=False)
+
+# reviews_score_good = df.filter(df.review_score == 1).select("review_text").limit(5)
+# print("review_score = 1:")
+# reviews_score_good.show(truncate=False)
 
 
-reviews_score_bad = df.filter(df.review_score == -1).select("review_text").limit(5)
-print("review_score = -1:")
-reviews_score_bad.show(truncate=False)
+# reviews_score_bad = df.filter(df.review_score == -1).select("review_text").limit(5)
+# print("review_score = -1:")
+# reviews_score_bad.show(truncate=False)
 
-start_time = time.time()
-filtered_df = df.filter(col("review_score") < 1)
-end_time = time.time()
-filtered_df.show()
-print(f"duree du traitement : {end_time - start_time:.4f} seconds")
+# start_time = time.time()
+# filtered_df = df.filter(col("review_score") < 1)
+# end_time = time.time()
+# filtered_df.show()
+# print(f"duree du traitement : {end_time - start_time:.4f} seconds")
